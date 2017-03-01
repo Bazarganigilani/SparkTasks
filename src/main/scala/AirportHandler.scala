9b129fd76583fb5f5d64b45c0b4ac8b353ba5421
root <root@ip-172-31-15-183.ap-southeast-2.compute.internal>
2017-03-01 22:50:01 +0000

    Explnations was added.

--- src/main/scala/AirportHandler.scala
+++ src/main/scala/AirportHandler.scala
@@ -17,15 +17,24 @@ object AirportHandler {
       //.enableHiveSupport()
       .getOrCreate()
 
+    //read the file
     val rowData=spark.sparkContext.textFile("/files/Flight_DataS.csv")
+    //read the originating airports
     val sourceAirports=rowData.map(x=>x.split(",")).map(x=> (x(16))).filter(_!="Origin")
+    //read the destination airports
     val destAirports=rowData.map(x=>x.split(",")).map(x=> (x(17))).filter(_!="Dest")
+    //union both originationg and destination airport RDDs
     val allAirports=spark.sparkContext.union(sourceAirports,destAirports)
+    //sum from above to extract bot innerand outer degreee of airports
     val scoredAirports= allAirports.map(x=>(x,1)).reduceByKey(_+_)//.map(x=>Row(x._1,x._2))
-    //scoredAirports.foreach(println)
+
     val orderingById: Ordering[(String,Int)] = Ordering.by(e => e._2)
+
+    //find the first airport
     val firstAirport=scoredAirports.max()(orderingById)
+    //the second airport is obtained using above when first airport is not considered
     val secondAirport=scoredAirports.filter(_._1!=firstAirport._1).max()(orderingById)
+    //find the third airport
     val thirdAirport=scoredAirports.filter(_._1!=firstAirport._1).filter(_._1!=secondAirport._1).max()(orderingById)
     println(s"The busiest airport is ${firstAirport._1} with ${firstAirport._2} flights.")
     println(s"The second busiest airport is ${secondAirport._1} with ${secondAirport._2} flights.")
